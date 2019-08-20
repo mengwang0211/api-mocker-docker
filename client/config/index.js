499da2d5ed0ebb914a44d82e278f8e2efce059ae
Meng Wang <wmlucas@163.com>
2019-08-20 08:45:15 +0800

    add index.js

--- client/config/index.js
+++ client/config/index.js
@@ -38,6 +38,6 @@ module.exports = {
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
     cssSourceMap: false,
-    serverRoot: '112.13.91.167:7001'
+    serverRoot: '112.13.91.102:7001'
   }
 }
